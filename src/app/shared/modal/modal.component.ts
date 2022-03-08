c1cbb29ea6dc04b6d3ca3f98501df5f739acd886
segfault0x61 <torresj1997@outlook.com>
2022-03-08 12:33:21 -0600

    Implement Modal Services/Components

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
+import { ModalService } from '../../services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
 export class ModalComponent implements OnInit {
+  @Input() modalID = '';
 
-  constructor() { }
+  constructor(public modal: ModalService, public el: ElementRef) {}
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
   }
 
+  closeModal() {
+    this.modal.toggleModal(this.modalID);
+  }
 }
