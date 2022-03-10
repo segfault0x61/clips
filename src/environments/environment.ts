ba1326cb57ed4364ec13ad89f1bdbf7d8f212935
segfault0x61 <torresj1997@outlook.com>
2022-03-10 11:26:38 -0600

    Setup Firebase SDK

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCYvQb0w_n-QFWyVfRhm69zisCJ2SXere0',
+    authDomain: 'clips-7f31a.firebaseapp.com',
+    projectId: 'clips-7f31a',
+    storageBucket: 'clips-7f31a.appspot.com',
+    appId: '1:1010383326661:web:fd0bb10b68db49547a4fac',
+  },
 };
 
 /*
