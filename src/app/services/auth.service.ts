323ca0fed668518b240ff5ec9752b9cd72aa2fbc
segfault0x61 <torresj1997@outlook.com>
2022-03-10 16:54:42 -0600

    Close Modal Upon Logging In

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,7 +4,7 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
-import { map, Observable } from 'rxjs';
+import { delay, map, Observable } from 'rxjs';
 
 import IUser from '../models/user.model';
 
@@ -14,11 +14,13 @@ import IUser from '../models/user.model';
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>;
 
-  isAuthenticated$: Observable<boolean>;
+  public isAuthenticated$: Observable<boolean>;
+  public isAuthenticatedWithDelay$: Observable<boolean>;
 
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
     this.usersCollection = db.collection('users');
     this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
   }
 
   public async createUser(userData: IUser) {
