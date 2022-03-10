9ddd35dd7c04a975fb47df6e1555c3c35c4e69e1
segfault0x61 <torresj1997@outlook.com>
2022-03-10 11:48:35 -0600

    User Registration with Firebase

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +8,10 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  constructor(private auth: AngularFireAuth) {}
+
+  inSubmission = false;
+
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
   email = new FormControl('', [Validators.required, Validators.email]);
   age = new FormControl('', [
@@ -38,9 +43,30 @@ export class RegisterComponent {
   alertMsg = 'Please wait! Your account is being created.';
   alertColor = 'blue';
 
-  register() {
+  async register() {
     this.showAlert = true;
     this.alertMsg = 'Please wait! Your account is being created.';
     this.alertColor = 'blue';
+    this.inSubmission = true;
+
+    const { email, password } = this.registerForm.value;
+
+    try {
+      const userCredentials = await this.auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(userCredentials);
+    } catch (e) {
+      console.error(e);
+
+      this.alertMsg = 'An unexpected error ocurred. Please try again later.';
+      this.alertColor = 'red';
+      this.inSubmission = false;
+      return;
+    }
+
+    this.alertMsg = 'Your account has been successfully created!';
+    this.alertColor = 'green';
   }
 }
