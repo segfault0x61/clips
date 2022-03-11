a2c32b42f75d7e0c769975a16b29aba309b59610
segfault0x61 <torresj1997@outlook.com>
2022-03-10 19:04:14 -0600

    Implement User Logout

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,11 @@ import { AuthService } from '../services/auth.service';
 export class NavComponent implements OnInit {
   isAuthenticated = false;
 
-  constructor(public modal: ModalService, public auth: AuthService) {
+  constructor(
+    public modal: ModalService,
+    public auth: AuthService,
+    private afAuth: AngularFireAuth
+  ) {
     this.auth.isAuthenticated$.subscribe((status) => {
       this.isAuthenticated = status;
     });
@@ -22,5 +27,11 @@ export class NavComponent implements OnInit {
     $event.preventDefault();
 
     this.modal.toggleModal('auth');
+  }
+
+  async logout($event: Event) {
+    $event.preventDefault();
+
+    await this.afAuth.signOut();
   }
 }
