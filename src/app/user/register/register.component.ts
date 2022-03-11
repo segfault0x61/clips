fdaba4ea6b5943d9af5f3c4af6ded6a0ac583ccf
segfault0x61 <torresj1997@outlook.com>
2022-03-10 23:24:42 -0600

    Add Error Message for Nonmatching Fields

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -40,7 +40,7 @@ export class RegisterComponent {
       confirm_password: this.confirm_password,
       phoneNumber: this.phoneNumber,
     },
-    [RegisterValidators.match]
+    [RegisterValidators.match('password', 'confirm_password')]
   );
 
   showAlert = false;
