6bf3fbeee8d3857c06b145ddb1fd4d283ac78268
segfault0x61 <torresj1997@outlook.com>
2022-03-11 19:07:00 -0600

    Subcribing to Route Params and Configuring Query Params

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ClipComponent } from './clip/clip.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'clip/:id', component: ClipComponent },
 ];
 
 @NgModule({
