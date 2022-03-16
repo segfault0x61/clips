d5f88d3d8f74307677094fa2349760c481f74425
segfault0x61 <torresj1997@outlook.com>
2022-03-15 21:57:21 -0500

    Add Screenshot Selections

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -30,6 +30,7 @@ export class UploadComponent implements OnDestroy {
   user: firebase.User | null = null;
   task?: AngularFireUploadTask;
   screenshots: string[] = [];
+  selectedScreenshot = '';
 
   title = new FormControl('', [Validators.required, Validators.minLength(3)]);
 
@@ -66,6 +67,8 @@ export class UploadComponent implements OnDestroy {
     }
 
     this.screenshots = await this.ffmpegService.getScreenshots(this.file);
+
+    this.selectedScreenshot = this.screenshots[0];
 
     this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
     this.nextStep = true;
